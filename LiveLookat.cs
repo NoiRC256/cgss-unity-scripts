9b067fe8df4e56c7b3ee69b6fe0e3614a4c75cf8
NoiRC256 <33998067+NoiRC256@users.noreply.github.com>
2019-12-17 15:41:15 +1100

    Update LiveLookat.cs

--- LiveLookat.cs
+++ LiveLookat.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /*
-☆☆☆ CGSS模型专用视线追踪脚本 （默认数值适配10倍模型大小的小早川紗枝 0132，0132_1011, 0132_1311 HQ）☆☆☆
-☆☆☆ 计算 Eye 与 Camera LookAt Handler 之间的 Vector3、Quaternion 差，得出相应x、w补偿值 ☆☆☆
-*需要将 Eye 设为 Eye Locator 的子骨骼，根据需要调整 Eye Locator 旋转
+☆☆☆ CGSS模型专用视线追踪脚本 ☆☆☆
+默认数值适配10倍大小的 Kobayakawa Sae 0132，0132_1011, 0132_1311 HQ 模型
+计算 Eye 与 Camera LookAt Handler 之间的 Vector3、Quaternion 差，输出相应的x、w补偿值
+*需要将 Eye 设为 Eye Locator 的子骨骼，按需调整 Eye Locator 旋转
+
 By NoiR_CCC
 */
 
@@ -39,9 +41,10 @@ public class LiveLookat : MonoBehaviour
     //计算相机位置
     public void calcCamPos()
     {
-        //==========变量区==========//
-        Vector3 camPos = LookAtHandler.transform.localPosition; // Camera LookAt Handler Position | 相机 世界转本地位置
-
+        //==========变量==========//
+        Vector3 camPos = LookAtHandler.transform.localPosition; // Camera LookAt Handler Position | 相机 世界位置→本地位置
+	
+	// Eye Locator calculations are unnecessary
         /*
         Vector3 eLOrigin = eyeLocatorL.transform.position; // Left Eye World Position | 左眼定位 世界位置
         Vector3 eROrigin = eyeLocatorR.transform.position; // Right Eye World Position |右眼定位 世界位置
@@ -59,7 +62,7 @@ public class LiveLookat : MonoBehaviour
         Quaternion camRotation = LookAtHandler.transform.rotation; // Camera LookAt Handler Rotation | 相机旋转
 
 
-        //==========功能区==========//
+        //==========功能==========//
    
         // Calculate Rotation difference between Right Eye and LookAt Handler | 计算右眼与相机之间的角度差
         Quaternion ReyeRotation = eyeLocatorR.transform.rotation; // 旧版
@@ -68,7 +71,7 @@ public class LiveLookat : MonoBehaviour
         
         Vector3 eRRelative = new Vector3( camPos.x - eyeRLocalOrigin.x - 1.5f * (1.015f + RighteyeRotationDiff.w), eyeRLocalOrigin.x - camPos.x, eyeRLocalOrigin.x - camPos.x); // Local Position difference between Right Eye and Camera LookAt Handler | 计算右眼与相机之间的x轴差
         Vector3 eRTarget = eRRelative / 175; //+ new Vector3(0.04776929f, 0.07901000f, 0.08240000f); // Calculate new target position | 右眼原位置 + x轴差距
-        eRTarget.x = Mathf.Clamp(eRTarget.x, -0.00785f, 0.00625f); // Clamp new target position | 限制移动范围 [内,外]
+        eRTarget.x = Mathf.Clamp(eRTarget.x, -0.00785f, 0.00625f); // Clamp new target position | 限制视线偏移范围 [内,外]
         Vector3 desR = new Vector3(eRTarget.x, eyeRight.transform.localPosition.y, eyeRight.transform.localPosition.z);
 
         eyeRight.transform.localPosition = desR; // Set new target position | 应用新位置
@@ -81,13 +84,13 @@ public class LiveLookat : MonoBehaviour
 
         Vector3 eLRelative = new Vector3( camPos.x - eyeLLocalOrigin.x - 1.5f * (0.985f + LefteyeRotationDiff.w), eyeLLocalOrigin.x - camPos.x, eyeLLocalOrigin.x - camPos.x); // Local Position difference between Left eye and Camera LookAt Handler | 计算左眼与相机之间的x轴差
         Vector3 eLTarget = eLRelative / 175; //+ new Vector3(-0.04776929f, 0.07901000f, 0.082400000f); // Calculate new target position | 左眼原位置 + x轴差距
-        eLTarget.x = Mathf.Clamp(eLTarget.x, -0.00625f, 0.00785f); // Clamp new target position | 限制移动范围 [外,内]
+        eLTarget.x = Mathf.Clamp(eLTarget.x, -0.00625f, 0.00785f); // Clamp new target position | 限制视线偏移范围 [外,内]
         Vector3 desL = new Vector3(eLTarget.x, eyeLeft.transform.localPosition.y, eyeLeft.transform.localPosition.z);
         
         eyeLeft.transform.localPosition = desL; // Set new target position | 应用新位置
         eyeLeft.transform.localPosition = Vector3.Lerp(eyeLeft.transform.localPosition, desL, Time.deltaTime * speed);
 
-        //==========试验区==========//
+        //==========测试==========//
 
         //Debug.Log("Right Eye X Modification: " + (eRTarget.x));
         Debug.Log("Left Eye X Modification: " + (eLTarget.x));
@@ -110,7 +113,7 @@ public class LiveLookat : MonoBehaviour
      */
     }
 
-    //==========应用区==========//
+    //==========应用==========//
     void Start ()
     {
         //calcCamPos();
